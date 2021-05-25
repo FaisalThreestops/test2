0ee150e57482cd8303bdfc4631aea065f010b030
FaisalThreestops <muhammed.faisal@threestops>
2021-05-25 23:03:21 +0530

    s

--- app/src/main/java/com/example/myapplications/MainActivity.java
+++ app/src/main/java/com/example/myapplications/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a=20;
+        int b=20;
     }
 }
